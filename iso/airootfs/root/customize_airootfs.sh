388782c8e5da152066f4383c041b3c20c2ce3a3d
adi1090x <adi1090x@gmail.com>
2021-06-04 22:47:49 +0530

    updated

--- iso/airootfs/root/customize_airootfs.sh
+++ iso/airootfs/root/customize_airootfs.sh
@@ -66,7 +66,7 @@ rm -rf /usr/lib/thunarx-3/thunar-wallpaper-plugin.so
 ## Hide Unnecessary Apps
 adir="/usr/share/applications"
 apps=(avahi-discover.desktop bssh.desktop bvnc.desktop compton.desktop echomixer.desktop \
-	envy24control.desktop exo-mail-reader.desktop exo-preferred-applications.desktop feh.desktop gparted.desktop \
+	envy24control.desktop exo-mail-reader.desktop exo-preferred-applications.desktop feh.desktop \
 	hdajackretask.desktop hdspconf.desktop hdspmixer.desktop hwmixvolume.desktop lftp.desktop \
 	libfm-pref-apps.desktop lxshortcut.desktop lstopo.desktop mimeinfo.cache \
 	networkmanager_dmenu.desktop nm-connection-editor.desktop pcmanfm-desktop-pref.desktop \
