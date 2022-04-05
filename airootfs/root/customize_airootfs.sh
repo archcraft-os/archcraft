25ee05c8506e3c0a99aeeed2231e85c0a3a10628
adi1090x <adi1090x@gmail.com>
2022-04-05 17:50:25 +0530

    Updated Package List

--- airootfs/root/customize_airootfs.sh
+++ airootfs/root/customize_airootfs.sh
@@ -146,7 +146,7 @@ apps=(avahi-discover.desktop bssh.desktop bvnc.desktop echomixer.desktop \
 	envy24control.desktop exo-preferred-applications.desktop feh.desktop \
 	hdajackretask.desktop hdspconf.desktop hdspmixer.desktop hwmixvolume.desktop lftp.desktop \
 	libfm-pref-apps.desktop lxshortcut.desktop lstopo.desktop \
-	networkmanager_dmenu.desktop nm-connection-editor.desktop pcmanfm-desktop-pref.desktop \
+	networkmanager_dmenu.desktop pcmanfm-desktop-pref.desktop \
 	qv4l2.desktop qvidcap.desktop stoken-gui.desktop stoken-gui-small.desktop thunar-bulk-rename.desktop \
 	thunar-settings.desktop thunar-volman-settings.desktop yad-icon-browser.desktop)
 
