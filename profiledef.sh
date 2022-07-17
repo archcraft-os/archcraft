1e929c8a6e74e99bfb3092ae77a2f2042f7461b4
adi1090x <adi1090x@gmail.com>
2022-07-17 13:28:52 +0530

    Updated the ISO base according to archiso 65-1

--- profiledef.sh
+++ profiledef.sh
@@ -8,7 +8,9 @@ iso_application="Archcraft Live/Installation/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
+           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
+           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
