ac9c45647ee25cfcb24cd27cd60545e01ed46401
adi1090x <adi1090x@gmail.com>
2025-07-12 16:07:05 +0530

    Updated ISO profiledef

--- profile/profiledef.sh
+++ profile/profiledef.sh
@@ -14,7 +14,9 @@ bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
-airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+#airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+# Fix xz error, `export XZ_OPT="--threads=2"`
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '512K' '-Xdict-size' '512K')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
